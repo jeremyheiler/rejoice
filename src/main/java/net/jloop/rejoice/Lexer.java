4ac919368ec88657756ec0f18292e3dde46953f7
Jeremy Heiler <jeremyheiler@gmail.com>
2021-04-26 20:20:51 -0400

    Enforce rules for symbols, quotes, and keywords

--- src/main/java/net/jloop/rejoice/Lexer.java
+++ src/main/java/net/jloop/rejoice/Lexer.java
@@ -17,8 +17,8 @@ import java.util.function.Consumer;
 
 public final class Lexer {
     private static final String whitespace = " \t\r\n";
-    private static final String adjacent = ".;()[]{}";
-    private static final String allow = "!$%&*+,-/0123456789<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ_`abcdefghijklmnopqrstuvwxyz|~:";
+    private static final String special = ";()[]{}";
+    private static final String allow = "!$%&*+.,-/0123456789<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ_`abcdefghijklmnopqrstuvwxyz|~";
 
     public Iterator<Token> map(ReaderIterator input) {
         class Mapper implements Iterator<Token>, Consumer<Token> {
@@ -64,9 +64,7 @@ public final class Lexer {
     private interface State {
         State consume(char character, Consumer<Token> collector);
 
-        default void complete(Consumer<Token> collector) {
-            // Do nothing
-        }
+        void complete(Consumer<Token> collector);
     }
 
     private static final class StartingState implements State {
@@ -85,48 +83,160 @@ public final class Lexer {
                 return new CharacterState();
             } else if (character == '^') {
                 return new TypeState();
-            } else if (contains(adjacent, character)) {
+            } else if (character >= '0' && character <= '9') {
+                return new IntegerState().consume(character, collector);
+            } else if (contains(special, character)) {
                 collector.accept(Token.of(Token.Type.Symbol, String.valueOf(character)));
                 return new StartingState();
             } else if (contains(allow, character)) {
-                return new SymbolOrIntegerState().consume(character, collector);
-            } else {
-                return this;
-            }
-        }
-    }
-
-    private static final class SymbolOrIntegerState implements State {
-        private final StringBuilder buffer = new StringBuilder();
-
-        // TODO: Match all printable characters.
-        // TODO: Symbols: Check for valid module names.
-        // TODO: Symbols: Check for multiple forward slashes.
-        // TODO: Symbols: Check for consecutive dots.
-
-        @Override
-        public State consume(char character, Consumer<Token> collector) {
-            if ((contains(whitespace, character) || contains(adjacent, character))) {
-                complete(collector);
-                return new StartingState().consume(character, collector);
-            } else if (contains(allow, character)) {
-                buffer.append(character);
+                return new SymbolState().consume(character, collector);
+            } else if (contains(whitespace, character)) {
                 return this;
             } else {
-                throw new RuntimeError("LEX", "Invalid symbol character: '" + character + "'");
+                throw new RuntimeError("LEX", "Unsupported character: '" + character + "'");
             }
         }
 
         @Override
         public void complete(Consumer<Token> collector) {
-            String lexeme = buffer.toString();
-            if (lexeme.matches("-?\\d+")) {
-                collector.accept(Token.of(Token.Type.Int, lexeme));
-            } else if (lexeme.equals("true") || lexeme.equals("false")) {
-                collector.accept(Token.of(Token.Type.Bool, lexeme));
+            // There's nothing to do, as this state could only collect whitespace.
+        }
+    }
+
+    private static final class NamedMetaState implements State {
+        private final StringBuilder buffer;
+        private final Token.Type type;
+
+        private boolean first = true;
+        private boolean slash = false;
+        private boolean dot = false;
+
+        public NamedMetaState(Token.Type type) {
+            this(type, new StringBuilder());
+        }
+
+        public NamedMetaState(Token.Type type, StringBuilder buffer) {
+            this.type = type;
+            this.buffer = buffer;
+        }
+
+        // TODO: Symbols: Check for valid module names.
+        // TODO: Match all printable characters.
+
+        @Override
+        public State consume(char character, Consumer<Token> collector) {
+            if ((contains(whitespace, character) || contains(special, character))) {
+                complete(collector);
+                return new StartingState().consume(character, collector);
+            } else if (contains(allow, character)) {
+                if (first) {
+                    first = false;
+                    if (character == '.') {
+                        throw new RuntimeError("LEX", "Invalid " + type.name().toLowerCase() + ": cannot start with a '.' character");
+                    }
+                }
+                if (character == '.') {
+                    if (dot) {
+                        throw new RuntimeError("LEX", "Invalid " + type.name().toLowerCase() + ": cannot have consecutive '.' characters");
+                    } else {
+                        dot = true;
+                    }
+                } else {
+                    dot = false;
+                }
+                if (character == '/') {
+                    if (slash) {
+                        throw new RuntimeError("LEX", "Invalid " + type.name().toLowerCase() + ": cannot have more than one '/' character");
+                    } else {
+                        slash = true;
+                    }
+                }
+                buffer.append(character);
+                return this;
             } else {
-                collector.accept(Token.of(Token.Type.Symbol, lexeme));
+                throw new RuntimeError("LEX", "Invalid " + type.name().toLowerCase() + ": disallowed character: '" + character + "'");
             }
+        }
+
+        @Override
+        public void complete(Consumer<Token> collector) {
+            collector.accept(Token.of(type, buffer.toString()));
+        }
+    }
+
+    private static final class SymbolState implements State {
+
+        @Override
+        public State consume(char character, Consumer<Token> collector) {
+            return new NamedMetaState(Token.Type.Symbol).consume(character, collector);
+        }
+
+        @Override
+        public void complete(Consumer<Token> collector) {
+            // There's nothing to do, as this state immediately delegates to another.
+        }
+    }
+
+    private static final class KeywordState implements State {
+        private final StringBuilder buffer = new StringBuilder().append(':');
+
+        @Override
+        public State consume(char character, Consumer<Token> collector) {
+            if ((contains(whitespace, character) || contains(special, character))) {
+                complete(collector);
+                return new StartingState().consume(character, collector);
+            } else {
+                return new NamedMetaState(Token.Type.Keyword, buffer).consume(character, collector);
+            }
+        }
+
+        @Override
+        public void complete(Consumer<Token> collector) {
+            collector.accept(Token.of(Token.Type.Symbol, buffer.toString()));
+        }
+    }
+
+    private static final class QuoteState implements State {
+        private final StringBuilder buffer = new StringBuilder().append('\'');
+
+        @Override
+        public State consume(char character, Consumer<Token> collector) {
+            if ((contains(whitespace, character) || contains(special, character))) {
+                throw new RuntimeError("LEX", "Incomplete quoted symbol");
+            } else if (character == '\'') {
+                buffer.append(character);
+                return this;
+            } else {
+                return new NamedMetaState(Token.Type.Quote, buffer).consume(character, collector);
+            }
+        }
+
+        @Override
+        public void complete(Consumer<Token> collector) {
+            throw new RuntimeError("LEX", "Incomplete quoted symbol");
+        }
+    }
+
+    private static final class IntegerState implements State {
+        private final StringBuilder buffer = new StringBuilder();
+
+        // TODO: Support negative integers
+        // TODO: What should be done with leading zeros?
+
+        @Override
+        public State consume(char character, Consumer<Token> collector) {
+            if (character >= '0' && character <= '9') {
+                buffer.append(character);
+                return this;
+            } else {
+                complete(collector);
+                return new StartingState().consume(character, collector);
+            }
+        }
+
+        @Override
+        public void complete(Consumer<Token> collector) {
+            collector.accept(Token.of(Token.Type.Int, buffer.toString()));
         }
     }
 
@@ -149,67 +259,6 @@ public final class Lexer {
         @Override
         public void complete(Consumer<Token> collector) {
             throw new RuntimeError("LEX", "Run-on string");
-        }
-    }
-
-    private static final class KeywordState implements State {
-        private final StringBuilder buffer = new StringBuilder().append(':');
-
-        // TODO: Ensure this matches symbols exactly.
-
-        @Override
-        public State consume(char character, Consumer<Token> collector) {
-            if ((contains(whitespace, character) || contains(adjacent, character))) {
-                complete(collector);
-                return new StartingState().consume(character, collector);
-            } else if (character == ':') {
-                throw new RuntimeError("LEX", "The colon character cannot be within a keyword");
-            } else {
-                buffer.append(character);
-                return this;
-            }
-        }
-
-        @Override
-        public void complete(Consumer<Token> collector) {
-            String lexeme = buffer.toString();
-            if (lexeme.equals(":")) {
-                collector.accept(Token.of(Token.Type.Symbol, lexeme));
-            } else {
-                collector.accept(Token.of(Token.Type.Keyword, lexeme));
-            }
-        }
-    }
-
-    private static final class QuoteState implements State {
-        private final StringBuilder buffer = new StringBuilder().append('\'');
-
-        private boolean doneQuoting = false;
-
-        // TODO: Ensure this matches symbols exactly.
-
-        @Override
-        public State consume(char character, Consumer<Token> collector) {
-            if ((contains(whitespace, character) || contains(adjacent, character))) {
-                complete(collector);
-                return new StartingState().consume(character, collector);
-            } else if (character == '\'') {
-                if (doneQuoting) {
-                    throw new RuntimeError("LEX", "The quote character cannot be within a symbol");
-                } else {
-                    buffer.append(character);
-                    return this;
-                }
-            } else {
-                doneQuoting = true;
-                buffer.append(character);
-                return this;
-            }
-        }
-
-        @Override
-        public void complete(Consumer<Token> collector) {
-            collector.accept(Token.of(Token.Type.Quote, buffer.toString()));
         }
     }
 
@@ -249,7 +298,7 @@ public final class Lexer {
 
         @Override
         public State consume(char character, Consumer<Token> collector) {
-            if ((contains(whitespace, character) || contains(adjacent, character))) {
+            if ((contains(whitespace, character) || contains(special, character))) {
                 complete(collector);
                 return new StartingState().consume(character, collector);
             } else {
@@ -274,7 +323,7 @@ public final class Lexer {
 
         @Override
         public State consume(char character, Consumer<Token> collector) {
-            if ((contains(whitespace, character) || contains(adjacent, character))) {
+            if ((contains(whitespace, character) || contains(special, character))) {
                 complete(collector);
                 return new StartingState().consume(character, collector);
 
